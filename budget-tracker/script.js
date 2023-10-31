861e995106f79c3b03b3a89f743c675f33ebae5d
Gabrielius Vaitkus <gabrieliusvaitkus.gv@gmail.com>
2023-10-31 10:18:17 +0000

    "Add functionality to add new entries to the budget tracker table"

--- budget-tracker/script.js
+++ budget-tracker/script.js
@@ -1,4 +1,36 @@
-//Event listener for the "Add" button
-document.getElementById('add').addEventListener('click', function(){
+const descriptionInput = document.getElementById('description');
+const amountInput = document.getElementById('amount');
+const typeSelect = document.getElementById('type');
+const addButton = document.getElementById('add');
+const entriesTableBody = document.querySelector('#entries tbody');
 
-});
+addButton.addEventListener('click', addEntry);
+
+function addEntry() {
+    const description = descriptionInput.value.trim();
+    const amount = parseFloat(amountInput.value);
+    const type = typeSelect.value;
+
+    if (!description || isNaN(amount)) {
+        alert('Please enter valid description and amount!');
+        return;
+    }
+
+    const newRow = entriesTableBody.insertRow();
+
+    const descriptionCell = newRow.insertCell(0);
+    const amountCell = newRow.insertCell(1);
+    const typeCell = newRow.insertCell(2);
+    const actionCell = newRow.insertCell(3);
+
+    descriptionCell.textContent = description;
+    amountCell.textContent = amount.toFixed(2);
+    typeCell.textContent = type;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function() {
+        entriesTableBody.removeChild(newRow);
+    });
+    actionCell.appendChild(deleteButton);
+}
