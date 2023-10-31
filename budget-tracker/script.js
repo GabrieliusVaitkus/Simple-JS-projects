03c3f3ef04dcc7e83090869d0735283e762a2151
Gabrielius Vaitkus <gabrieliusvaitkus.gv@gmail.com>
2023-10-31 13:29:57 +0000

    Finished basic functions, started on canvas

--- budget-tracker/script.js
+++ budget-tracker/script.js
@@ -4,6 +4,10 @@ const amountInput = document.getElementById('amount');
 const typeSelect = document.getElementById('type');
 const addButton = document.getElementById('add');
 const entriesTableBody = document.querySelector('#entries tbody');
+const totalIncomeDisplay = document.getElementById('totalIncomeDisplay');
+const totalExpensesDisplay = document.getElementById('totalExpensesDisplay');
+const balanceDisplay = document.getElementById('balanceDisplay');
+const ctx = document.getElementById('budgetChart').getContext('2d');
 //Event listener for the "Add" button to trigger the "addEntry" function
 addButton.addEventListener('click', addEntry);
 //Function to handle the logic when "Add" is clicked
@@ -27,7 +31,7 @@ function addEntry() {
     //Populate cells with the user input
     descriptionCell.textContent = description;
     amountCell.textContent = amount.toFixed(2);
-    typeCell.textContent = type;
+    typeCell.textContent = type.charAt(0).toUpperCase() + type.slice(1);
     //Creation of "Delete" button, so the user would be able to delete an entry
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
@@ -36,4 +40,33 @@ function addEntry() {
         entriesTableBody.removeChild(newRow);
     });
     actionCell.appendChild(deleteButton);
+    updateDisplay();
+}
+//Update the display "updateDisplay" functions will calculate the total income, expenses and balance based on the entries.
+function updateDisplay() {
+    const rows = document.querySelectorAll("#entries tbody tr");
+    let totalIncome = 0;
+    let totalExpenses = 0;
+
+    rows.forEach(row => {
+        const amount = parseFloat(row.cells[1].textContent);
+        const type = row.cells[2].textContent;
+
+        if (type === "Income") {
+            totalIncome += amount;
+        } else if (type === "Expense") {
+            totalExpenses += amount;
+        }
+    });
+    
+    const balance = totalIncome - totalExpenses;
+
+    totalIncomeDisplay.textContent = totalIncome.toFixed(2);
+    totalExpensesDisplay.textContent = totalExpenses.toFixed(2);
+    balanceDisplay.textContent = balance.toFixed(2);
+}
+updateDisplay();
+
+function renderChart(income, expenses) {
+    
 }
